e3926c5c2d0e56029c538e639cd28e6824a52994
Valentina <lucenkovalentina@yandex.ru>
2024-01-06 20:08:33 +0300

    add metods in class router, part 2

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -28,12 +28,58 @@ class Router
 
     }
 
-    //
+    //принимает запрос
     public static function dispatch($url)
     {
-       var_dump($url);
+       if(self::matchRoute($url)) {
+            echo 'OK';
+       }else {
+        echo 'NO';
+       }
 
     }
 
+    //сравниваем поступившый запрос с шаблоном регулярного выражения
+    public static function matchRoute($url):bool
+    {
+        foreach(self::$routes as $pattern => $route) {
 
+            if(preg_match("~{$pattern}~", $url, $mathes)) {
+                foreach($mathes as $k => $v) {
+                    if(is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                 }
+
+                if(!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                }else {
+                    $route['admin_prefix'] = '\\';
+                }
+                $route['controller'] = self::upperCamelCase($route['controller']); 
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //преобразует action в CamelCase
+    protected static function upperCamelCase($str) 
+    {
+        $str = str_replace('-', '', $str);
+        $str = ucwords($str);
+        $str = str_replace(' ', '', $str);
+
+        return $str;
+    }
+
+     //преобразует action в camelCase
+     protected static function lowerCamelCase($str) 
+     {
+        return $str = lcfirst(self::upperCamelCase($str));
+     }
 }
